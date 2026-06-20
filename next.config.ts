8a32c0b7f115f8dd6726a1fca0106b5a766cd9cb
DexenRoss <emiliocaballero@ciencias.unam.mx>
2026-06-20 14:07:56 -0600

    fix: next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: isProduction ? "/emilio-portfolio" : "",
+  assetPrefix: isProduction ? "/emilio-portfolio/" : "",
   images: {
     unoptimized: true,
   },
