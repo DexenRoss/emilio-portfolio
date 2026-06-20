e230878954b5e100dd5b81140fccb6e025a3f2a1
DexenRoss <emiliocaballero@ciencias.unam.mx>
2026-06-20 14:16:42 -0600

    fix: isGithubActions

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
 import type { NextConfig } from "next";
 
-const isProduction = process.env.NODE_ENV === "production";
+const isGithubActions = process.env.GITHUB_ACTIONS === "true";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: isProduction ? "/emilio-portfolio" : "",
-  assetPrefix: isProduction ? "/emilio-portfolio/" : "",
+  basePath: isGithubActions ? "/emilio-portfolio" : "",
+  assetPrefix: isGithubActions ? "/emilio-portfolio/" : "",
   images: {
     unoptimized: true,
   },
